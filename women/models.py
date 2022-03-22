aefe291799b20cc631c187e8b65f92a70f1480c4
Glen <gulyashshsh@gmail.com>
2022-03-22 16:59:42 +0600

    ViewSets

--- women/models.py
+++ women/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -15,6 +16,7 @@ class Women(models.Model):
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
     cat = models.ForeignKey(
         'Category', on_delete=models.PROTECT, verbose_name='Категория')
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- women/views.py
+++ women/views.py
@@ -1,27 +1,89 @@
-from django.forms import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from women.serializers import *
 from .models import *
 
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        queryset = Women.objects.all()
-        return Response({'posts': WomenSerlializer(queryset, many=True).data})
+# ---------------------------------------------------------------------------
 
-    def post(self, request):
-        serializer = WomenSerlializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
 
-        new_post = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': WomenSerlializer(new_post).data})
+class WomenViewSet(viewsets.ModelViewSet):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+
+        if not pk:
+            return Women.objects.filter(is_published=True)
+
+        return Women.objects.filter(pk=pk, is_published=True)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
+
+    @action(methods=['get'], detail=False)
+    def categories(self, request):
+        cats = Category.objects.all()
+        return Response({'cats': [c.name for c in cats]})
+
+# ---------------------------------------------------------------------------
+
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+
+# ---------------------------------------------------------------------------
+
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         queryset = Women.objects.all()
+#         return Response({'posts': WomenSerializer(queryset, many=True).data})
+
+#     def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#         return Response({'post': serializer.data})
+
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk', None)
+#         if not pk:
+#             return Response({"error": "Method PUT is not allowed"})
+
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exist"})
+
+#         serializer = WomenSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
+
+    # def delete(self, request, *args, **kwargs):
+    #     pk = kwargs.get('pk', None)
+    #     if not pk:
+    #         return Response({"error": "Method PUT is not allowed"})
+
+    #     return Response({'post': "delete post" + str(pk)})
 
 
 # class WomenAPIView(generics.ListAPIView):
